904a7fca735c43286defea5bb8c8e72917fb7e35
Paola Andrea <paolaandrea@Jhon-2.local>
2020-02-20 22:15:50 -0500

    agregue un for

--- src/HolaMundo.java
+++ src/HolaMundo.java
@@ -3,5 +3,11 @@ public class HolaMundo{
 	public static void main(String[] args){
 
 		System.out.println("Hola Mundo");
+
+		for (int i = 0; i<5 ; i++ ) {
+
+			System.out.println("Hola mundo" + i + " veces ");
+			
+		}
 	}
 }
